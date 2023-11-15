a55389b28d34f76a46c8d35717306d66e0d0801b
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2023-11-15 17:28:29 +0100

    fix unit test

--- app/util/blockaid/index.ts
+++ app/util/blockaid/index.ts
@@ -11,10 +11,11 @@ export const isBlockaidFeatureEnabled = () =>
 export const getBlockaidMetricsParams = (
   securityAlertResponse?: SecurityAlertResponse,
 ) => {
-  const additionalParams: Record<string, any> = {};
-  additionalParams.security_alert_reason = Reason.notApplicable;
+  const additionalParams: Record<string, any> = {};  
 
   if (securityAlertResponse && isBlockaidFeatureEnabled()) {
+    additionalParams.security_alert_reason = Reason.notApplicable;
+    
     const { result_type, reason, providerRequestsCount } =
       securityAlertResponse;
 
